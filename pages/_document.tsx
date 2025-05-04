6e602efe930c4a7d428a2e6662a2652c1a7e70be
Abdul-Raheem324 <abdulraheemwork324@gmail.com>
2025-05-04 11:56:13 +0530

    new commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <title>HabitSpark | A best habit tracker app</title>
       <Head />
       <body className="antialiased">
         <Main />
